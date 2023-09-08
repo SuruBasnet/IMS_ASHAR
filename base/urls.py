f0ef30f2b463a904ce220bf62833f37026f36f5e
SuruBasnet <surubasnet824@gmail.com>
2023-09-08 08:33:50 +0545

    crud operation

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('company_info/',CompanyInfoApiView.as_view(),name='home')
+    path('company_info/',CompanyInfoApiView.as_view(),name='company info'),
+    path('company_info/<int:pk>/',CompanyInfoIdApiView.as_view(),name='company info detail')
 ]
--- base/views.py
+++ base/views.py
@@ -14,8 +14,49 @@ from rest_framework.generics import GenericAPIView
 
 class CompanyInfoApiView(GenericAPIView):
     queryset = CompanyInfo.objects.all()
+    serializer_class = CompanyInfoSerializer
 
     def get(self,request):
         company_info_objects = self.get_queryset()
         serializer = CompanyInfoSerializer(company_info_objects,many=True)
         return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = CompanyInfoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response('Data created!')
+        else:
+            return Response(serializer.errors)
+        
+class CompanyInfoIdApiView(GenericAPIView):
+    queryset = CompanyInfo.objects.all()
+    serializer_class = CompanyInfoSerializer
+
+    def get(self,request,pk):
+        try:
+            company_info_object = CompanyInfo.objects.get(id=pk)
+        except:
+            return Response('Data not found!')
+        serializer = CompanyInfoSerializer(company_info_object)
+        return Response(serializer.data)
+    
+    def put(self,request,pk):
+        try:
+            company_info_object = CompanyInfo.objects.get(id=pk)
+        except:
+            return Response('Data not found!')
+        serializer = CompanyInfoSerializer(company_info_object,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response('Data updated successfully')
+        else:
+            return Response(serializer.errors)
+        
+    def delete(self,request,pk):
+        try:
+            company_info_object = CompanyInfo.objects.get(id=pk)
+        except:
+            return Response('Data not found!')
+        company_info_object.delete()
+        return Response('Data deleted successfully!')
